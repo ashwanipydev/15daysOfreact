597050420f74373fd0495d972845c1307e403fe6
Ashwani Pandey <110717013+ashwanipydev@users.noreply.github.com>
2023-10-22 14:41:33 +0000

    07_reatrouter

--- 07_reactRouter/src/main.jsx
+++ 07_reactRouter/src/main.jsx
@@ -7,6 +7,7 @@ import Home from './components/Home/Home.jsx'
 import About from './components/About/About.jsx'
 import Contact from './components/contact/Contact.jsx'
 import User from './components/User/User.jsx'
+import Github, { githubInfoLoader } from './components/Github/Github.jsx'
 
 /**  v1
  * const router = createBrowserRouter([
@@ -41,6 +42,7 @@ const router = createBrowserRouter(
       <Route path='about' element={<About />} />
       <Route path='contact' element={<Contact />} />
       <Route path='user/:userid' element={<User />} />
+      <Route loader={githubInfoLoader} path='github' element={<Github />} />
     </Route>
   )
 )
